6d5b5ef8c6abf103a950a5d8992944e9cb8b5dc2
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-11-29 14:59:09 +0600

    State management with react

---
+++ sample-counter/src/App.js
@@ -0,0 +1,19 @@
+import { Counter } from "./components/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+        <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+          Simple Counter Application
+        </h1>
+
+        <div class="max-w-md mx-auto mt-10 space-y-5">
+          <Counter/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
